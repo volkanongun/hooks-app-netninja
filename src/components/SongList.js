e2b80d72a5753cf2c744088e6b952f0d5ac5d223
Volkan Ongun <me@volkanongun.com>
2020-05-10 16:30:33 +0300

    basic project initialized

---
+++ src/components/SongList.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import uuid from 'uuid/v1'
+
+const SongList = () => {
+
+  const [songs, setSongs] = useState([
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 }
+  ])
+
+  const addSong = () => {
+  	setSongs([...songs, {title : "new song", id : 4} ])
+  }
+
+	return (
+		<div className="song-list">
+			<ul>
+				{songs.map(song=> <li key={uuid()}>{song.title}</li>)}
+			</ul>
+			<button onClick={addSong}>Add A Song</button>
+		</div>
+	)
+}
+
+export default SongList
